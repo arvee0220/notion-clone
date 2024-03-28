e4c3e8b6a4a07f00172f4048e50977b7daf4d81a
Rowell Verdejo <rowellverdejo@gmail.com>
2024-03-28 07:32:55 -0400

    Create user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Page } from './Page/Page';
 import { AppStateProvider } from './state/AppStateContext';
 import { createPage } from './utils/createPage';
 import { Route, Routes } from 'react-router-dom';
+import { Auth } from './auth/Auth';
+import { Private } from './auth/Private';
 
 const initialState = createPage();
-
-const Auth = () => <div>Auth</div>;
 
 function App() {
     return (
@@ -14,17 +14,25 @@ function App() {
             <Route
                 path='/:id'
                 element={
-                    <AppStateProvider initialState={initialState}>
-                        <Page />
-                    </AppStateProvider>
+                    <Private
+                        component={
+                            <AppStateProvider initialState={initialState}>
+                                <Page />
+                            </AppStateProvider>
+                        }
+                    />
                 }
             />
             <Route
                 path='/'
                 element={
-                    <AppStateProvider initialState={initialState}>
-                        <Page />
-                    </AppStateProvider>
+                    <Private
+                        component={
+                            <AppStateProvider initialState={initialState}>
+                                <Page />
+                            </AppStateProvider>
+                        }
+                    />
                 }
             />
         </Routes>
